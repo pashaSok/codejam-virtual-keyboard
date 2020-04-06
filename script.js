503eab2794cff7b714c0670d45c7fdc8f90cde51
pashaSok <https://github.com/pashaSok>
2020-04-07 00:08:14 +0300

    fix: add some fix on keyboard and keyup keydown

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let body = document.getElementsByTagName('body')[0];
 
+let lang = 'en';
+let Caps = false;
+
 const keys = [
     [{ value: '`', code: 192 }, { value: '1', code: 49 }, { value: '2', code: 50 }, { value: '3', code: 51 }, { value: '4', code: 52 }, { value: '5', code: 53 }, { value: '6', code: 54 }, { value: '7', code: 55 }, { value: '8', code: 56 }, { value: '9', code: 57 }, { value: '0', code: 48 }, { value: '-', code: 189 },{value:'=',code:187},{value:'Backspace',code:8}],
     [{ value: 'Tab', code: 9 }, { value: 'q', code: 81 }, { value: 'w', code: 87 }, { value: 'e', code: 69 }, { value: 'r', code: 82 }, { value: 't', code: 84 }, { value: 'y', code: 89 }, { value: 'u', code: 85 }, { value: 'i', code: 73 }, { value: 'o', code: 79 }, { value: 'p', code: 80 }, { value: '[', code: 219 },{value:']',code:221},{value:'\\',code:220},{value:'Del',code:46}],
@@ -28,53 +31,63 @@ keyboardWrapper.setAttribute('class', 'keyboard');
 container.appendChild(textArea);
 container.appendChild(keyboardWrapper);
 
-keys.forEach(line=>{
-    let lines = document.createElement('div');
-    keyboardWrapper.appendChild(lines);
-    lines.setAttribute('class','lines');
-    line.forEach(key=>{
-        let button = document.createElement('div');
-        lines.appendChild(button);
-		button.setAttribute('class','button');
-		button.textContent=key.value;
-        if(key.code==8 || key.code == 20 || key.code ==16 || key.code == 13){
-            button.classList.add('func-button');
-        }
-        if(key.code==32){
-            button.classList.add('space');
-		}		
-        button.addEventListener('mousedown',(e)=>{
-            button.classList.add('active');
-            if(key.code == 9){
-                textArea.value+='  ';
-            }
-            else if(key.code == 8){
-				textArea.value=textArea.value.slice(0,-1);
+if(lang == 'en' && Caps == false){
+	keys.forEach(line=>{
+		let lines = document.createElement('div');
+		keyboardWrapper.appendChild(lines);
+		lines.setAttribute('class','lines');
+		line.forEach(key=>{
+			let button = document.createElement('div');
+			lines.appendChild(button);
+			button.setAttribute('class','button');
+			button.textContent=key.value;
+			if(key.code==8 || key.code == 20 || key.code ==16 || key.code == 13){
+				button.classList.add('func-button');
 			}
-			else if(key.code == 20){
+			if(key.code==32){
+				button.classList.add('space');
+			}		
+			button.addEventListener('mousedown',(e)=>{
 				button.classList.add('active');
-			}
-			else {
-				textArea.value+=key.value;
-			}
-		});
-        button.addEventListener('mouseup',()=>{
-            button.classList.remove('active');
-		});
-
-		window.addEventListener('keydown',(e)=>{	
-			if(button.textContent==e.key){
-				button.classList.add('active');
-				textArea.value+=e.key;
-			}
-		});
-		window.addEventListener('keyup',(e)=>{	
-			if(button.textContent==e.key){
+				if(key.code == 9){
+					textArea.value+='  ';
+				}
+				else if(key.code == 8){
+					textArea.value=textArea.value.slice(0,-1);
+				}
+				else if(key.code == 20){
+					button.classList.add('active');
+				}
+				else {
+					textArea.value+=key.value;
+				}
+			});
+			button.addEventListener('mouseup',()=>{
 				button.classList.remove('active');
-				textArea.value+=e.key;
-			}
+			});
+	
+			window.addEventListener('keydown',(e)=>{	
+				if(button.textContent==e.key){
+					button.classList.add('active');
+					if(e.keyCode == 8){
+						textArea.value=textArea.value.slice(0,-1);
+					}
+					else if(key.code == 9){
+						event.preventDefault();
+						textArea.value+='  ';
+					}
+					 else if(key.code!=8) {
+						 textArea.value+=button.textContent;
+					}
+				}
+			});
+			window.addEventListener('keyup',(e)=>{	
+				if(button.textContent==e.key){
+					button.classList.remove('active');
+				}
+			});
 		});
 	});
-});
+}
 
 
